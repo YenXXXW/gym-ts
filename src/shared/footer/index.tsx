2d9c946fc9dbfb53c52806441d376fd7cabbf1a9
Yen <YenXXXW@gmail.com>
2023-07-19 11:40:02 +0630

    unused imports remved

--- src/shared/footer/index.tsx
+++ src/shared/footer/index.tsx
@@ -1,11 +1,6 @@
-import { SelectedPage } from '../types'
 import Logo from "@/assets/Logo.png";
 
-type Props = {
-    setSelectedPage: (value: SelectedPage) => void
-}
-
-const Footer = (setSelectedPage: Props) => {
+const Footer = () => {
   return <footer className='bg-primary-100 py-16'>
     <div className='justify-content mx-auto w-5/6 gap-16 md:flex'>
       <div className='mt-16 basis-1/2 md:mt-0'>
